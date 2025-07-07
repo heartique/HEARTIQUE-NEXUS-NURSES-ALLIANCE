63bb45bdb9deb362ec57842ff46919192d5b9a32
HEARTIQUE NEXUS <brianmuthomi851@gmail.com>
2025-07-07 23:58:13 +0300

    Update service-worker.js
    add

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,46 @@
+const CACHE_NAME = 'heartique-cache-v1';
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png',
+  '/style.css', // if you have one
+  '/script.js'  // if you have one
+];
+
 self.addEventListener('install', event => {
-  console.log('[ServiceWorker] Installed');
+  console.log('[ServiceWorker] Installing...');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('[ServiceWorker] Pre-caching offline files');
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
-  console.log('[ServiceWorker] Activated');
+  console.log('[ServiceWorker] Activating...');
+  event.waitUntil(
+    caches.keys().then(keyList =>
+      Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME) {
+            console.log('[ServiceWorker] Removing old cache:', key);
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
